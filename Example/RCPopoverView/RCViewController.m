d3952fec6e27e33c770689a8c1a61d17192aa039
whyefa <whyefa@163.com>
2018-07-02 09:52:12 +0800

    使用示例

--- Example/RCPopoverView/RCViewController.m
+++ Example/RCPopoverView/RCViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RCViewController.h"
-@interface RCViewController ()
+#import <RCPopoverView/RCPopoverView.h>
 
+@interface RCViewController ()
+@property (nonatomic, assign) CGFloat screenwidth;
+@property (nonatomic, assign) CGFloat screenHeight;
 @end
 
 @implementation RCViewController
@@ -16,19 +19,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.screenwidth = UIScreen.mainScreen.bounds.size.width;
+    self.screenHeight = UIScreen.mainScreen.bounds.size.height;
 }
 
 - (IBAction)popDefaultMenu:(id)sender {
+    RCPopoverView *popoverMenu = [RCPopoverView popoverView];
+    popoverMenu.style = RCPopoverViewStyleDefault;
+    popoverMenu.arrowStyle = RCPopoverViewArrowStyleRound;
+    [popoverMenu showToPoint:CGPointMake(20, 50) withActions:[self menuItems]];
 }
 
 - (IBAction)popDarkMenu:(id)sender {
+    RCPopoverView *popoverMenu = [RCPopoverView popoverView];
+    popoverMenu.style = RCPopoverViewStyleDark;
+    popoverMenu.arrowStyle = RCPopoverViewArrowStyleTriangle;
+    [popoverMenu showToPoint:CGPointMake(self.screenwidth-40, 50) withActions:[self menuItems]];
 }
 
 - (IBAction)popShadowMenu:(id)sender {
+    RCPopoverView *popoverMenu = [RCPopoverView popoverView];
+    popoverMenu.arrowStyle = RCPopoverViewArrowStyleTriangle;
+    popoverMenu.borderStyle = RCPopoverViewBorderStyleShadow;
+    popoverMenu.shadowParam = (RCPopoverShadow){0.4, 3, CGSizeZero};
+    popoverMenu.shadowColor = [UIColor colorWithWhite:0 alpha:0.5];
+    [popoverMenu showToPoint:CGPointMake(20, self.screenHeight-40) withActions:[self menuItems]];
 }
 - (IBAction)popBorderMenu:(id)sender {
+    RCPopoverView *popoverMenu = [RCPopoverView popoverView];
+    popoverMenu.borderStyle = RCPopoverViewBorderStyleBoarder;
+    popoverMenu.separatorColor = UIColor.greenColor;
+    [popoverMenu showToPoint:CGPointMake(self.screenwidth-40, self.screenHeight-40) withActions:[self menuItems]];
 }
 
+- (NSArray *)menuItems {
+    RCPopoverAction *action1 = [RCPopoverAction actionWithTitle:@"分享到微信" handler:^(RCPopoverAction *action) {
+        NSLog(@"分享到微信");
+    }];
+    RCPopoverAction *action2 = [RCPopoverAction actionWithTitle:@"分享到QQ" handler:^(RCPopoverAction *action) {
+        NSLog(@"分享到QQ");
+    }];
+    RCPopoverAction *action3 = [RCPopoverAction actionWithTitle:@"退出登录" handler:^(RCPopoverAction *action) {
+        NSLog(@"退出登录");
+    }];
+    return @[action1, action2, action3];
+}
 
 @end
